9bc69013941acb574b9ba9dc57cc334b5e2b0b70
uvaceka-cmyk <uvacek.a@gmail.com>
2026-09-09 14:41:50 +0200

    Delete app directory

--- app/ui/SiteLink.tsx
+++
@@ -1,9 +0,0 @@
-import type { AnchorHTMLAttributes } from "react";
-
-type Props = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href"> & {
-  href: string;
-};
-
-export default function SiteLink({ href, children, ...props }: Props) {
-  return <a href={href} {...props}>{children}</a>;
-}
